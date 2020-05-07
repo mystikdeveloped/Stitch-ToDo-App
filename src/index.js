33f83d459ba6230ac0b22618ea7685d3bf6ca462
mystikdeveloped <42699943+mystikdeveloped@users.noreply.github.com>
2020-05-07 01:30:22 -0400

    Removed App ID to make repo Public

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
